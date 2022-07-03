f0d7f7bc7fddd412b56e070826436c6ce70831c6
Machfar <souhayel.isetch@gmail.com>
2022-07-03 18:47:09 +0100

    fixed routes

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const userRouter = require("./routes/userRoutes.js");
 
 // Middleware
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static(__dirname + "/public"));
 
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 // Routes
-app.use("/users", userRouter);
+app.get("/api/v1/", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "API docs",
+  });
+});
+
+app.use("/api/v1/users", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,11 +37,11 @@ app.listen(PORT, () => {
 app.all("*", (req, res, next) => {
   res.status(400).json({
     status: "Error",
-    message: "undefined route",
+    message: "unhandled route",
   });
 });
 
-// Error first midleware
+// Error first middleware
 app.use((err, req, res, next) => {
   res.status(400).json({
     status: "Error",
